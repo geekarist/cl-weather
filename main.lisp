ec3e7855348d8a622165e4dbe27ca22666ea9872
cp <geekarist@users.noreply.github.com>
2025-12-28 22:49:44 +0100

    Reindent using spaces

--- main.lisp
+++ main.lisp
@@ -4,29 +4,29 @@
 (defun view (model)
   '(:page-header
     (:home-button (:icon :TBD
-		   :on-click (:type :type-navigation
-			      :destination :destination-home))
+                   :on-click (:type :type-navigation
+                              :destination :destination-home))
      :today-button (:text "Auxerre, Yonne, -1°C"
-		    :on-click (:message-type :navigation
-			       :navigation-destination :destination-today)))
-
+                    :on-click (:type :type-navigation
+                               :destination :destination-today)))
+    
     :page-content
     (:today-block (:title "TODAY'S WEATHER"
-		   :subtitle "SAT, DEC 27"
-		   :prediction-high (:icon :icon-sun-behind-small-cloud
-				     :desc "Sunny to partly cloudy"
-				     :temp "Hi: 7°")
-		   :prediction-low (:icon :icon-moon-behind-small-cloud
-				    :desc "Tonight: Mainly clear and cold"
-				    :temp "Lo: -2°"))
+                   :subtitle "SAT, DEC 27"
+                   :prediction-high (:icon :icon-sun-behind-small-cloud
+                                     :desc "Sunny to partly cloudy"
+                                     :temp "Hi: 7°")
+                   :prediction-low (:icon :icon-moon-behind-small-cloud
+                                    :desc "Tonight: Mainly clear and cold"
+                                    :temp "Lo: -2°"))
      :current-block (:title "CURRENT WEATHER"
-		     :subtitle "6:17 PM"
-		     :icon :icon-moon-behind-small-cloud
-		     :temp (:actual "-1°"
-			    :real-feel "-1°"
-			    :unit "C")
-		     :desc "Mostly clear"
-		     :details (:TBD))))))
+                     :subtitle "6:17 PM"
+                     :icon :icon-moon-behind-small-cloud
+                     :temp (:actual "-1°"
+                            :real-feel "-1°"
+                            :unit "C")
+                     :desc "Mostly clear"
+                     :details (:TBD))))))
 
 (defun update (model message)
   :TBD)
