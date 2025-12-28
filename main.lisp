e289ce3d6ced85d2fc63ecf3866ded34f8ad856e
cp <geekarist@users.noreply.github.com>
2025-12-28 11:17:33 +0100

    More view

--- main.lisp
+++ main.lisp
@@ -3,21 +3,30 @@
 
 (defun view (model)
   '(:page-header
-    (:home-button (:icon :icon-home
-		   :on-click (:message-type :destination-home))
+    (:home-button (:icon :TBD
+		   :on-click (:type :type-navigation
+			      :destination :destination-home))
      :today-button (:text "Auxerre, Yonne, -1°C"
-		    :on-click (:message-type :destination-today)))
+		    :on-click (:message-type :navigation
+			       :navigation-destination :destination-today)))
 
     :page-content
     (:today-block (:title "TODAY'S WEATHER"
 		   :subtitle "SAT, DEC 27"
-		   :prediction-high (:icon :TBD
+		   :prediction-high (:icon :icon-sun-behind-small-cloud
 				     :desc "Sunny to partly cloudy"
 				     :temp "Hi: 7°")
-		   :prediction-low (:icon :TBD
+		   :prediction-low (:icon :icon-moon-behind-small-cloud
 				    :desc "Tonight: Mainly clear and cold"
 				    :temp "Lo: -2°"))
-     :current-block :TBD))))
+     :current-block (:title "CURRENT WEATHER"
+		     :subtitle "6:17 PM"
+		     :icon :icon-moon-behind-small-cloud
+		     :temp (:actual "-1°"
+			    :real-feel "-1°"
+			    :unit "C")
+		     :desc "Mostly clear"
+		     :details (:TBD))))))
 
 (defun update (model message)
   :TBD)
