4eb7826638b745afb5674f5e9e7ced05901aef7c
cp <geekarist@users.noreply.github.com>
2026-02-03 23:29:37 +0100

    Command, structs, update

--- main.lisp
+++ main.lisp
@@ -1,24 +1,17 @@
+(defparameter +commands+
+  `(:fx-get-decoded-time
+    ,(lambda (dispatch)
+       (funcall dispatch `(:msg-type :msg-got-current-time
+                           :msg-value ,(multiple-value-list
+                                        (get-decoded-time)))))))
+
 (defun init ()
-  '(:location (:city "Auxerre" :department "Yonne")
-    :conditions-current (:temperature (:value -1
-                                       :unit :unit-deg-celsius)
-                         :cover :cover-sunny-partly-cloudy
-                         :air-quality 75)
-    :conditions-today (:low (:temperature (:value -4
-                                           :unit :unit-deg-celsius)
-                             :summary "Clearing and cold"
-                             :cover :cover-intermittent-clouds)
-                       :high  (:temperature (:value 2
-                                             :unit :unit-deg-celsius)
-                               :cover :cover-partly-sunny-w-flurries
-                               :summary "Tomorrow: a morning flurry; otherwise, chilly with sunshine and a few clouds"
-                               :air-quality :TBD
-                               :pressure :TBD
-                               :precipitation))))
+  '(:model nil
+    :command :fx-get-decoded-time))
 
 (defparameter +strings+
   '(:unit-deg-celsius "°C"
-    :label-today-subtitle "TODAY'S WEATHER"))
+    :label-today-title "TODAY'S WEATHER"))
 
 (defun view (model)
   (let* ((model-location (getf model :location))
@@ -35,8 +28,8 @@
                                     (getf model-location :department)
                                     model-conditions-current-temp-value
                                     unit-str))
-         (today-block-title (getf +strings+ :label-today-subtitle))
-         (today-block-subtitle (getf model-conditions-today-date)))
+         (today-block-title (getf +strings+ :label-today-title))
+         (today-block-subtitle (format nil "~A" (getf model :date)))) 
     `(:header (:home-button (:icon :icon-app-logo
                              :on-click (:type :type-navigation
                                         :destination :destination-home))
@@ -45,7 +38,7 @@
                                          :destination :destination-today)))
       :content
       (:today-block (:title ,today-block-title
-                     :subtitle "SAT, DEC 27"
+                     :subtitle ,today-block-subtitle
                      :high (:icon :icon-sun-behind-small-cloud
                             :desc "Sunny to partly cloudy"
                             :temp "Hi: 7°")
@@ -66,5 +59,54 @@
                                   :value "Poor"
                                   :color :color-yellow)))))))
 
+(defstruct change model command)
+
+(defstruct model date location conditions-current conditions-today command)
+
+(defstruct command name arguments)
+
+(defstruct message type payload)
+
+(defstruct location city department)
+
+(defstruct conditions-today low high)
+
+(defstruct conditions
+  temperature
+  cover
+  summary
+  air-quality
+  pressure
+  precipitation)
+
+(defun copy-struct-with (src-struct &rest slot-value-pairs)
+  (let ((dest-struct (copy-structure src-struct)))
+    (loop while slot-value-pairs
+          do (let ((slot (pop slot-value-pairs))
+                   (value (pop slot-value-pairs)))
+               (setf (slot-value dest-struct slot) value)))
+    dest-struct))
+
 (defun update (model message)
-  :TBD)
+  (case (getf message :msg-type)
+    (:msg-got-current-time
+     `(:model
+       (:date ,(getf message :msg-value)
+        :location (:city "Auxerre" :department "Yonne")
+        :conditions-current (:temperature (:value -1
+                                           :unit :unit-deg-celsius)
+                             :cover :cover-sunny-partly-cloudy
+                             :air-quality 75)
+        :conditions-today
+              (:low (:temperature (:value -4 :unit :unit-deg-celsius)
+                     :summary "Clearing and cold"
+                     :cover :cover-intermittent-clouds)
+               :high (:temperature (:value 2 :unit :unit-deg-celsius)
+                      :cover :cover-partly-sunny-w-flurries
+                      :summary "Tomorrow: a morning flurry; otherwise, chilly with sunshine and a few clouds"
+                      :air-quality :TBD
+                      :pressure :TBD
+                      :precipitation :TBD)))
+       :command nil))
+    (otherwise `(:model ,model
+                 :command nil))))
