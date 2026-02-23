f21279d8e5dc29691890384ddc8fe0f61fc22006
cp <geekarist@users.noreply.github.com>
2026-02-23 18:36:06 +0100

    Simple runtime to manage initial change
    - Get location from web service
    - Get time
    - Initial change
    - Structs for view
    - Update on current time message
    - Simple main function

---
+++ cl-weather.asd
@@ -0,0 +1,7 @@
+(defsystem "cl-weather"
+  :description "Weather app"
+  :version "0.0.1"
+  :depends-on ("drakma")
+  :components ((:file "main")))
+
+
--- main.lisp
+++ main.lisp
@@ -1,76 +1,45 @@
-(defparameter +commands+
-  `(:fx-get-decoded-time
-    ,(lambda (dispatch)
-       (funcall dispatch `(:msg-type :msg-got-current-time
-                           :msg-value ,(multiple-value-list
-                                        (get-decoded-time)))))))
+(ql:quickload '(:drakma
+                #:com.inuoe.jzon))
+
+(setf drakma:*text-content-types*
+      '(("text" . nil)
+        ("application" . "json")))
+
+(defun get-current-location ()
+  (let* ((_ (drakma:http-request "https://wtfismyip.com/json")))
+    _))
 
 (defun init ()
-  '(:model nil
-    :command :fx-get-decoded-time))
+  (make-change
+   :model nil
+   :command (lambda (dispatch)
+              (funcall dispatch
+                       (make-current-time-message
+                        :value (get-decoded-time)))
+              (funcall dispatch
+                       (make-current-location-message
+                        :value (get-current-location))))))
 
 (defparameter +strings+
   '(:unit-deg-celsius "°C"
     :label-today-title "TODAY'S WEATHER"))
 
+(defstruct view header content)
+(defstruct view-header home-button today-button)
+(defstruct view-icon-button icon on-click)
+(defstruct view-text-button text on-click)
+(defstruct view-nav-action destination)
+(defstruct view-content today current)
+
 (defun view (model)
-  (let* ((model-location (getf model :location))
-         (model-conditions-current (getf model :conditions-current))
-         (model-conditions-current-temp
-           (getf model-conditions-current :temperature))
-         (model-conditions-current-temp-unit
-           (getf model-conditions-current-temp :unit))
-         (model-conditions-current-temp-value
-           (getf model-conditions-current-temp :value))
-         (unit-str (getf +strings+ model-conditions-current-temp-unit))
-         (today-button-text (format nil "~A, ~A, ~D~A"
-                                    (getf model-location :city)
-                                    (getf model-location :department)
-                                    model-conditions-current-temp-value
-                                    unit-str))
-         (today-block-title (getf +strings+ :label-today-title))
-         (today-block-subtitle (format nil "~A" (getf model :date)))) 
-    `(:header (:home-button (:icon :icon-app-logo
-                             :on-click (:type :type-navigation
-                                        :destination :destination-home))
-               :today-button (:text ,today-button-text
-                              :on-click (:type :type-navigation
-                                         :destination :destination-today)))
-      :content
-      (:today-block (:title ,today-block-title
-                     :subtitle ,today-block-subtitle
-                     :high (:icon :icon-sun-behind-small-cloud
-                            :desc "Sunny to partly cloudy"
-                            :temp "Hi: 7°")
-                     :low (:icon :icon-moon-behind-small-cloud
-                           :desc "Tonight: Mainly clear and cold"
-                           :temp "Lo: -2°"))
-       :current-block (:title "CURRENT WEATHER"
-                       :subtitle "6:17 PM"
-                       :icon :icon-moon-behind-small-cloud
-                       :temp (:actual "-1°"
-                              :real-feel "-1°"
-                              :unit "C")
-                       :desc "Mostly clear"
-                       :details ((:label "RealFeel Shade™" :value "-2°")
-                                 (:label "Wind" :value "NNE 9 km/h")
-                                 (:label "Wind Gusts" :value "21 km/h")
-                                 (:label "Air Quality"
-                                  :value "Poor"
-                                  :color :color-yellow)))))))
+  :TBD)
 
 (defstruct change model command)
-
-(defstruct model date location conditions-current conditions-today command)
-
-(defstruct command name arguments)
-
-(defstruct message type payload)
-
+(defstruct model date location observations-current predictions-today command)
+(defstruct command id arguments)
 (defstruct location city department)
-
-(defstruct conditions-today low high)
-
+(defstruct predictions-day low high)
+;;; Conditions hold the data for both observations and predictions
 (defstruct conditions
   temperature
   cover
@@ -78,6 +47,8 @@
   air-quality
   pressure
   precipitation)
+(defstruct current-time-message value)
+(defstruct current-location-message value)
 
 (defun copy-struct-with (src-struct &rest slot-value-pairs)
   (let ((dest-struct (copy-structure src-struct)))
@@ -88,25 +59,31 @@
     dest-struct))
 
 (defun update (model message)
-  (case (getf message :msg-type)
-    (:msg-got-current-time
-     `(:model
-       (:date ,(getf message :msg-value)
-        :location (:city "Auxerre" :department "Yonne")
-        :conditions-current (:temperature (:value -1
-                                           :unit :unit-deg-celsius)
-                             :cover :cover-sunny-partly-cloudy
-                             :air-quality 75)
-        :conditions-today
-              (:low (:temperature (:value -4 :unit :unit-deg-celsius)
-                     :summary "Clearing and cold"
-                     :cover :cover-intermittent-clouds)
-               :high (:temperature (:value 2 :unit :unit-deg-celsius)
-                      :cover :cover-partly-sunny-w-flurries
-                      :summary "Tomorrow: a morning flurry; otherwise, chilly with sunshine and a few clouds"
-                      :air-quality :TBD
-                      :pressure :TBD
-                      :precipitation :TBD)))
-       :command nil))
-    (otherwise `(:model ,model
-                 :command nil))))
+  (cond
+    ((current-time-message-p message)
+     (update-from-current-time model message))
+    (:otherwise (make-change :model model :command nil))))
+
+(defun update-from-current-time (model message)
+  (make-change
+   :model (copy-struct-with model
+                            'date (current-time-message-value message))
+   :command nil))
+
+(defun main ()
+  (let*
+      ((initial-change (init))
+       (_ (print (format nil "Initial change: ~S" initial-change)))
+       (initial-command (change-command initial-change))
+       (_ (print (format nil "Initial command: ~S" initial-command)))
+       (initial-model (change-model initial-change))
+       (_ (print (format nil "Initial model: ~S" initial-model)))
+       (_ (print "Invoking initial command"))
+       (_ (funcall initial-command
+                   (lambda (&rest args)
+                     (print (format
+                             nil
+                             "Initial command invoked with arguments: ~S"
+                             args))))))))
+
+
